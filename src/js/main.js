335b3e967c9028df489e98948a76145e129fc48b
Khamitov Timur <ricomen@mail.ru>
2017-05-09 12:36:49 +0500

    add section abot adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,58 @@
-//jQuery begin
-$(document).ready(function() {
+'use strict';
+(function() {
+  var mainNavToggle = document.querySelector(".main-nav__toggle");
+  var mainNav       = document.querySelector(".main-nav__list");
+  // console.log(mainNav);
 
+  mainNavToggle.addEventListener("click", function() {
+
+    mainNav.classList.toggle("main-nav__list--show");
+
+  });
+
+})();
+
+  function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+  r(function(){
+    if (!document.getElementsByClassName) {
+        // Поддержка IE8
+        var getElementsByClassName = function(node, classname) {
+            var a = [];
+            var re = new RegExp('(^| )'+classname+'( |$)');
+            var els = node.getElementsByTagName("*");
+            for(var i=0,j=els.length; i<j; i++)
+                if(re.test(els[i].className))a.push(els[i]);
+            return a;
+        }
+        var videos = getElementsByClassName(document.body,"youtube");
+    } else {
+        var videos = document.getElementsByClassName("youtube");
+    }
+
+    var nb_videos = videos.length;
+    for (var i=0; i<nb_videos; i++) {
+        // Зная идентификатор видео на YouTube, легко можно найти его миниатюру
+        videos[i].style.backgroundImage = 'url(http://i.ytimg.com/vi/' + videos[i].id + '/sddefault.jpg)';
+
+        // Добавляем иконку Play поверх миниатюры, чтобы было похоже на видеоплеер
+        var play = document.createElement("div");
+        play.setAttribute("class","play");
+        videos[i].appendChild(play);
+
+        videos[i].onclick = function() {
+            // создаем iframe со включенной опцией autoplay
+            var iframe = document.createElement("iframe");
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            if (this.getAttribute("data-params")) iframe_url+='&'+this.getAttribute("data-params");
+            iframe.setAttribute("src",iframe_url);
+            iframe.setAttribute("frameborder",'0');
+
+            // Высота и ширина iframe должны быть такими же, как и у родительского блока
+            iframe.style.width  = this.style.width;
+            iframe.style.height = this.style.height;
+
+            // Заменяем миниатюру плеером с YouTube
+            this.parentNode.replaceChild(iframe, this);
+        }
+    }
 });
-$(window).load(function() {
-  /* Act on the event */
-});
-//jQuery end
-(function($){
-  
-})(jQuery);
